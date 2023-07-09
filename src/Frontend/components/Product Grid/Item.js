d5fc1327f229a859034d1f5ebf64b83b82335ad7
mikhail <74957842+mikmik01@users.noreply.github.com>
2023-07-09 22:05:58 +0800

    Delete src directory

--- src/Frontend/components/Product Grid/Item.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Card from "@mui/material/Card";
-import "./Item.css";
-
-const Item = ({ text }) => {
-  return <Card className="card">{text}</Card>;
-};
-
-export default Item;
--- src/Frontend/components/Welcome Page/Welcome.js
+++
@@ -1,7 +0,0 @@
-import "./Welcome.css";
-
-const Welcome = () => {
-  return <div className="welcome">This will be the welcome page</div>;
-};
-
-export default Welcome;
