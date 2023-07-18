d53a55ea8b5da96f163b53649233e2d1242f57f8
VurhD <benegaldhruv2312@gmail.com>
2023-07-18 19:22:36 +0800

    The Backend folder should have the next.js app

---
+++ backend/keeble/models/user.js
@@ -0,0 +1,22 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, 'Email already exists'],
+        required: [true, 'Email is required'],
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+    },
+    image: {
+        type: String,
+    }
+
+});
+
+// Check models to see if a user model already exists:
+const User = models.User || model("User", UserSchema);
+
+export default User;
