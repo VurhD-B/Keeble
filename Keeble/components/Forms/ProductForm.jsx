46b509d3daaa41700f3811692693ba644c4736e0
Chua Zhi Yun <42872909+czyun44@users.noreply.github.com>
2023-07-23 21:57:46 +0800

    Update for img upload
    Rmb to npm install

--- Keeble/components/Forms/ProductForm.jsx
+++ Keeble/components/Forms/ProductForm.jsx
@@ -1,7 +1,9 @@
-
 import Link from "next/link";
+import "@uploadthing/react/styles.css";
 
-const ProductForm = ({type, product, setProduct, submitting, handleSubmit}) => {
+import { UploadButton } from "/utils/uploadthing";
+
+const ProductForm = ({ type, product, setProduct, submitting, handleSubmit }) => {
     return (
         <section className="w-50 flex-start flex-col mx-8 my-4 bg-carolina-blue rounded-xl px-10 py-6">
             <h1 className="bold text-left">
@@ -11,77 +13,98 @@ const ProductForm = ({type, product, setProduct, submitting, handleSubmit}) => {
 
                 <label className="flex flex-col gap-2">
                     <span className="font-bold">Product Name:</span>
-                    <textarea 
-                        value={product.name} 
-                        onChange={(e) => setProduct({...product, name: e.target.value})} 
+                    <textarea
+                        value={product.name}
+                        onChange={(e) => setProduct({ ...product, name: e.target.value })}
                         placeholder="Enter the product name here..."
                         className="px-2 py-2 rounded-xl shadow-md"
-                        required/>
+                        required />
                 </label>
 
                 <label className="flex flex-col gap-2">
                     <span className="font-bold">Product Description:</span>
-                    <textarea 
-                        value={product.description} 
-                        onChange={(e) => setProduct({...product, description: e.target.value})} 
+                    <textarea
+                        value={product.description}
+                        onChange={(e) => setProduct({ ...product, description: e.target.value })}
                         placeholder="Enter the product description here..."
                         className="px-2 py-2 rounded-xl shadow-md"
-                        required/>
+                        required />
                 </label>
 
                 <label className="flex flex-col gap-2">
                     <span className="font-bold">Product Price:</span>
-                    <input 
+                    <input
                         type="number"
-                        value={product.price} 
-                        onChange={(e) => setProduct({...product, price: e.target.value})} 
+                        value={product.price}
+                        onChange={(e) => setProduct({ ...product, price: e.target.value })}
                         placeholder="Enter the product price here..."
                         className="px-2 py-2 rounded-xl shadow-md"
-                        required/>
+                        required />
                 </label>
 
                 <label className="flex flex-col gap-2">
                     <span className="font-bold">Product Primary Category:</span>
-                    <input 
-                        value={product.category1} 
-                        onChange={(e) => setProduct({...product, category1: e.target.value})} 
+                    <input
+                        value={product.category1}
+                        onChange={(e) => setProduct({ ...product, category1: e.target.value })}
                         placeholder="Enter the primary category here..."
                         className="px-2 py-2 rounded-xl shadow-md"
-                        required/>
+                        required />
                 </label>
 
                 <label className="flex flex-col gap-2">
                     <span className="font-bold">Product Secondary Category:</span>
-                    <input 
-                        value={product.category2} 
-                        onChange={(e) => setProduct({...product, category2: e.target.value})} 
+                    <input
+                        value={product.category2}
+                        onChange={(e) => setProduct({ ...product, category2: e.target.value })}
                         placeholder="Enter the secondary category here..."
                         className="px-2 py-2 rounded-xl shadow-md"
-                        required/>
+                        required />
                 </label>
 
                 <label className="flex flex-col gap-2">
-                    <span className="font-bold">Product Link: 
-                    <span className="font-light">(Add the link to the site that sells it)</span>
+                    <span className="font-bold">Product Link:
+                        <span className="font-light">(Add the link to the site that sells it)</span>
                     </span>
-                    <input 
-                        value={product.productLink} 
-                        onChange={(e) => setProduct({...product, productLink: e.target.value})} 
+                    <input
+                        value={product.productLink}
+                        onChange={(e) => setProduct({ ...product, productLink: e.target.value })}
                         placeholder="Enter the product link name here..."
                         className="px-2 py-2 rounded-xl shadow-md"
-                        required/>
+                        required />
                 </label>
 
                 <label className="flex flex-col gap-2">
-                    <span className="font-bold">Image Link: 
-                    <span className="font-light">(Add the link to s3 URL where image is stored)</span>
+                    <span className="font-bold">Image Link:
+                        <span className="font-light">(Add the link to s3 URL where image is stored)</span>
                     </span>
-                    <input 
-                        value={product.imageLink} 
-                        onChange={(e) => setProduct({...product, imageLink: e.target.value})} 
+
+                    <UploadButton
+                        endpoint="imageUploader"
+                        onClientUploadComplete={(res) => {
+                            // Do something with the response
+                            console.log("Files: ", res);
+                            console.log("Files: ", res[0].fileUrl);
+                            setProduct({ ...product, imageLink: res[0].fileUrl })
+                            alert("Upload Completed");
+                        }}
+                        onUploadError={(error) => {
+                            // Do something with the error.
+                            alert(`ERROR! ${error.message}`);
+                        }}
+                    />
+                    <img
+                        src={product.imageLink}
+                        alt='profile-image'
+                    />
+
+
+                    <input
+                        value={product.imageLink}
+                        onChange={(e) => setProduct({ ...product, imageLink: e.target.value })}
                         placeholder="Enter the Image link name here..."
                         className="px-2 py-2 rounded-xl shadow-md"
-                        required/>
+                        required />
                 </label>
 
                 <div className="flex gap-4 justify-between mt-5">
@@ -92,7 +115,7 @@ const ProductForm = ({type, product, setProduct, submitting, handleSubmit}) => {
                         type="submit"
                         disabled={submitting}
                         className="text-lg bg-mulled-wine text-titan-white rounded-xl px-2 py-1">
-                            {submitting ? `${type}...` : type}
+                        {submitting ? `${type}...` : type}
                     </button>
                 </div>
             </form>
