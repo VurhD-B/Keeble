1c92cb78e89969c8a20d4aa8a5e924d3dda484b8
VurhD <benegaldhruv2312@gmail.com>
2023-07-29 03:54:58 +0800

    Implemented the POST and GET endpoints for Builds

---
+++ Keeble/app/api/builds/route.js
@@ -0,0 +1,14 @@
+import { connectToDB } from "@utils/database";
+import Build from "@models/build";
+
+export const GET = async (req) => {
+    try {
+        await connectToDB();
+        const builds = await Build.find({}).populate("parts");
+        return new Response(JSON.stringify(builds), {status: 200});
+    } catch (error) {
+        return new Response('Failed to fetch the builds', {status: 500});
+    }
+    
+
+}
