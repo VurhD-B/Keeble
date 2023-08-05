5251d5bc9a03cc47dc748f7232367e51325655c1
VurhD <benegaldhruv2312@gmail.com>
2023-08-05 16:58:20 +0800

    Clean up

--- Keeble/app/productlist/page.jsx
+++ Keeble/app/productlist/page.jsx
@@ -23,16 +23,20 @@ const ProductList = () => {
 
     return (
         <div className="flex flex-col justify-center items-center">
+            
+            {/* Search Bar */}
             <div className="flex items-center bg-text-white rounded-lg shadow-md p-2 mt-5">
                 <input
                     type="text"
                     placeholder="Search for products..."
-                    className="flex-grow outline-none px-4 py-2 text-gray-700"
+                    className="flex-grow outline-none px-4 py-2 text-gray-700 w-96"
                 />
                 <button className="ml-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
                     Search
                 </button>
             </div>
+
+            {/* Products Grid */}
             <div className="flex flex-row flex-wrap my-10 mx-10 gap-5">
                 {products.map((product) => {
                     return (
