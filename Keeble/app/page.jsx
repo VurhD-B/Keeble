20c07da3f1580e1b5893133f97b20bb896e532e7
VurhD <benegaldhruv2312@gmail.com>
2023-08-06 11:59:38 +0800

    Clean up of home page

--- Keeble/app/page.jsx
+++ Keeble/app/page.jsx
@@ -7,37 +7,11 @@ export default function Home() {
   const { data:session } = useSession();
   return (
     <section className="flex justify-center items-center gap-4 py-10">
-      <Link href="/productbuild">
-        <button className="button ">
-          Customise your own keyboard!
-        </button>
-      </Link>
       <Link href="/add">
         <button className="button">
           Add Products
         </button>
       </Link>
-      <Link href="/guide">
-        <button className="button">
-          Guide Page
-        </button>
-      </Link>
-      <Link href="/productlist">
-        <button className="button">
-          Product List Page
-        </button>
-      </Link>
-      {session?.user ?
-      (
-        <Link href="/posts">
-          <button className="button">
-            Posts Page
-          </button>
-        </Link>
-      ) : (
-        <>
-        </>
-      )}
     </section>
   )
 }
