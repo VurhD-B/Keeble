1a7299025fa7b331a9a8579bf59316c019318d42
VurhD <benegaldhruv2312@gmail.com>
2023-08-06 11:52:12 +0800

    Individual product page perfected

--- Keeble/components/ReviewContainer.jsx
+++ Keeble/components/ReviewContainer.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const ReviewContainer = ({review}) => {
     return (
-        <div className='w-[75%] bg-review-box-gray rounded-lg px-5 py-5 flex flex-col gap-3 object-contain'>
+        <div className='w-[100%] bg-review-box-gray rounded-lg px-5 py-5 flex flex-col gap-3 object-contain'>
             <div className='flex items-center gap-5'>
                 <Image src={review.user.image} width={45} height={45} alt="profile-image" className='rounded-full'/>
                 <h1 className='text-text-white'>{review.user.email}</h1>
