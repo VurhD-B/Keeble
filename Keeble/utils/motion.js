2179374fb9a49f2ed2819fa116fc4789445e4a73
VurhD <benegaldhruv2312@gmail.com>
2023-08-08 01:11:03 +0800

    Final

--- Keeble/utils/motion.js
+++ Keeble/utils/motion.js
@@ -1,20 +1,3 @@
-export const slideIn = (direction, type, delay, duration) => ({
-    hidden: {
-        x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0,
-        y: direction === 'up' ? '100%' : direction === 'down' ? '100%' : 0,
-    },
-    show: {
-        x: 0,
-        y: 0,
-        transition: {
-            type,
-            delay,
-            duration,
-            ease: 'easeOut',
-        },
-    },
-});
-
 export const staggerContainer = (staggerChildren, delayChildren) => ({
     hidden: {},
     show: {
