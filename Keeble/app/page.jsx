3c3a96552929465842b0dbb188d7c829ef3b0232
VurhD <benegaldhruv2312@gmail.com>
2023-08-07 22:11:10 +0800

    Home page animations

--- Keeble/app/page.jsx
+++ Keeble/app/page.jsx
@@ -1,17 +1,24 @@
 'use client';
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { motion } from 'framer-motion';
+import { staggerContainer } from "@utils/motion";
+import TypingText from "@components/TypingText";
 
 
 export default function Home() {
   const { data:session } = useSession();
   return (
     <section className="flex justify-center items-center gap-4 py-10">
-      <Link href="/add">
-        <button className="button">
-          Add Products
-        </button>
-      </Link>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25}}
+        className="m-auto mt-44"
+      >
+        <TypingText title="Your Keeb journey starts here.." textStyles="text-center"/>
+      </motion.div>
     </section>
   )
 }
--- Keeble/utils/motion.js
+++ Keeble/utils/motion.js
@@ -7,10 +7,45 @@ export const slideIn = (direction, type, delay, duration) => ({
         x: 0,
         y: 0,
         transition: {
-        type,
-        delay,
-        duration,
-        ease: 'easeOut',
+            type,
+            delay,
+            duration,
+            ease: 'easeOut',
         },
     },
 });
+
+export const staggerContainer = (staggerChildren, delayChildren) => ({
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren,
+            delayChildren,
+        },
+    },
+});
+
+export const textContainer = {
+    hidden: {
+        opacity: 0,
+    },
+    show: (i = 1) => ({
+        opacity: 1,
+        transition: { staggerChildren: 0.1, delayChildren: i * 0.5 * 0.1 },
+    }),
+};
+
+export const textVariant2 = {
+    hidden: {
+        opacity: 0,
+        y: 20,
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'tween',
+            ease: 'easeIn',
+        },
+    },
+};
