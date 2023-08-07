8392dcf4c0da4c07e399c99289e4c79d9fc18af0
maquilino.2022 <mikhailaquilino@gmail.com>
2023-08-08 01:14:13 +0800

    resolve conflict

--- Keeble/components/Containers/BuildProductContainer.jsx
+++ Keeble/components/Containers/BuildProductContainer.jsx
@@ -5,18 +5,18 @@ import Image from "next/image";
 const ProductContainer = ({ children, product, btnaction, btnactionfunc }) => {
     return (
         <div className='flex flex-col gap-2 bg-box-gray w-full h-[90%] max-h-240 max-w-96 overflow-hidden rounded-xl
-        shadow-xl hover:bg-background-black hover:text-text-white hover:rounded-xl'>
+                    shadow-xl hover:bg-background-black hover:text-text-white hover:rounded-xl'>
             <div className="w-60 h-32 overflow-hidden place-self-center items-center">
                 <Image width={240} height={120} className="object-cover w-full h-full" src={product.imageLink}/>
             </div>
-            <div className=" flex flex-col mt-2 justify-center items-center">
+            <div className=" flex flex-col mt-2 -mb-4 justify-center items-center">
                 <h1>{product.name}</h1>
             </div>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col mb-4 justify-center items-center">
                 {
                     btnaction ? 
                         <button 
-                            className="button bg-button-blue mt-14 hover:bg-button-blue-hover rounded-xl p-2 text-base" 
+                            className="button bg-button-blue mt-8 hover:bg-button-blue-hover rounded-xl p-2 text-base" 
                             onClick={() => {
                                 btnactionfunc(product);
                             }}>
