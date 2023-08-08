28d56277cc897d127c69d9ee97084112fc9ff66c
VurhD <benegaldhruv2312@gmail.com>
2023-08-08 10:48:38 +0800

    API enhancement

--- Keeble/app/api/reviews/[productId]/route.js
+++ Keeble/app/api/reviews/[productId]/route.js
@@ -5,7 +5,7 @@ export const GET = async (req, { params }) => {
     try {
         await connectToDB();
         const reviews = await Review.find({product: params.productId}).populate('user');
-        return new Response(JSON.stringify(reviews, {status: 200}));
+        return new Response(JSON.stringify(reviews), {status: 200});
     } catch (error) {
         return new Response('Failed to fetch reviews', {status: 500});
     }
